439f610605f76850198769d80f0ea537c3127829
thanhledh1 <thanhle.vt2@gmail.com>
2024-01-24 08:28:30 +0700

    first commit

---
+++ inter.js
@@ -0,0 +1,3 @@
+var numbers = [1, 2, 3, 4, 5];
+var firstNumber = numbers[0];
+console.log(firstNumber);
---
+++ inter.ts
@@ -0,0 +1,8 @@
+interface MyArray {
+    [index: number]: number;
+  }
+  
+  let numbers: MyArray = [1, 2, 3, 4, 5];
+  let firstNumber: number = numbers[0];
+  console.log(firstNumber);
+  
